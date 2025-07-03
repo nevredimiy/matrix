a240c020c1a32a12d873a2010d632ac93621abc0
nevredimiy <artemlitvinov81@gmail.com>
2025-07-03 20:10:17 +0300

    Исправить разметку страницы создания заказа, заменив компонент на правильный и добавив кнопку для отправки формы

--- resources/views/filament/pages/production-order-builder.blade.php
+++ resources/views/filament/pages/production-order-builder.blade.php
@@ -1,3 +1,7 @@
-<x-filament-panels::page>
+<x-filament::page>
+    {{ $this->form }}
 
-</x-filament-panels::page>
+    <x-filament::button wire:click="submit">
+        Создать заказ
+    </x-filament::button>
+</x-filament::page>
