7b2d7ce0a6da29b4ea72cba7b6770e90680d2f6f
nevredimiy <artemlitvinov81@gmail.com>
2025-07-06 10:58:50 +0300

    Исправить опечатку в свойстве $fillable модели Product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    protected $fillabel = [
+    protected $fillable = [
         'name',
         'sku',
         'stock_quantity', // количество 
