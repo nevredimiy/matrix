7fb4b6d52332ac7170496b07952db2ad953d2ec3
nevredimiy <artemlitvinov81@gmail.com>
2025-07-10 17:53:47 +0300

    Убрал лишние пробельчики

--- app/Filament/Resources/ProductResource/Pages/ListProducts.php
+++ app/Filament/Resources/ProductResource/Pages/ListProducts.php
@@ -8,10 +8,6 @@ use Filament\Resources\Pages\ListRecords;
 use Filament\Actions\Action;
 use App\Models\Product;
 use Filament\Notifications\Notification;
-use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\Cache;
-use GuzzleHttp\Client;
-use GuzzleHttp\Cookie\CookieJar;
 use App\Models\OcProduct;
 use Illuminate\Support\Facades\DB;
 
@@ -78,90 +74,7 @@ class ListProducts extends ListRecords
                         ->send();
                 }),
 
-
-
             Actions\CreateAction::make(),
-
-            // Action::make('import_from_opencart')
-            //     ->label('Импорт из OpenCart по Api')
-            //     ->icon('heroicon-o-arrow-down-tray')
-            //     ->color('success')
-            //     ->requiresConfirmation()
-            //     ->action(function () {
-
-            //         $baseUrl = 'https://dinara.david-freedman.com.ua/';
-            //         $apiKey = env('OC_DINARA_API');
-            //                             $cookieJar = new \GuzzleHttp\Cookie\CookieJar();
-
-            //         $client = new Client([
-            //             'base_uri' => $baseUrl,
-            //             'cookies' => $cookieJar,
-            //         ]);
-
-            //         $loginResponse = $client->post('index.php?route=api/login', [
-            //             'form_params' => ['key' => $apiKey],
-            //         ]);
-
-            //         $body = json_decode((string) $loginResponse->getBody(), true);
-            //         $apiToken = $body['api_token'] ?? null;
-
-            //         if (!$apiToken) {
-            //             dd('Ошибка логина', $body);
-            //         }
-
-            //         // ⬇️ вот тут — проверим, какие cookies сохранены
-            //         foreach ($cookieJar->toArray() as $cookie) {
-            //             echo $cookie['Name'] . ': ' . $cookie['Value'] . "\n";
-            //         }
-
-            //         // Тот же клиент, тот же cookieJar
-            //         $response = $client->get('index.php?route=api/product/getProducts', [
-            //             'query' => ['api_token' => $apiToken],
-            //             'headers' => [
-            //                 'Accept'     => 'application/json',
-            //             ]
-            //     ]);
-
-
-            //         $body = (string) $response->getBody();
-
-            //         // 👇 Добавим на этом этапе dd, чтобы увидеть реальный ответ
-
-            //         $data = json_decode($body, true);
-
-            //         if (!isset($data['products']) || !is_array($data['products'])) {
-            //             Notification::make()
-            //                 ->title('Нет данных о продуктах')
-            //                 ->warning()
-            //                 ->send();
-            //             return;
-            //         }
-
-            //         // 5. Сохраняем продукты
-            //         foreach ($data['products'] as $product) {
-            //             Product::updateOrCreate(
-            //                 ['model' => $product['model']],
-            //                 [
-            //                     'name' => $product['name'] ?? 'Без названия',
-            //                     'ean' => $product['ean'] ?? null,
-            //                     'price' => $product['price'] ?? 0,
-            //                     'quantity' => $product['quantity'] ?? 0,
-            //                     'status' => $product['status'] ?? 0,
-            //                 ]
-            //             );
-            //         }
-
-            //         Notification::make()
-            //             ->title('Импорт завершен: ' . count($data['products']) . ' товаров')
-            //             ->success()
-            //             ->send();
-
-            //     }),
-
-
-
-
-
         ];
     }
 }
