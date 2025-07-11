5d691246df04bbacfbbe45563ed0cbac91602d46
nevredimiy <artemlitvinov81@gmail.com>
2025-07-11 19:04:10 +0300

    Доработки 3

--- app/Filament/Resources/OrderStatusResource/Pages/ListOrderStatuses.php
+++ app/Filament/Resources/OrderStatusResource/Pages/ListOrderStatuses.php
@@ -9,6 +9,7 @@ use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 use Filament\Actions\Action;
 use Filament\Notifications\Notification;
+use Illuminate\Support\Facades\DB;
 
 class ListOrderStatuses extends ListRecords
 {
@@ -21,57 +22,50 @@ class ListOrderStatuses extends ListRecords
             Action::make('update_order_statuses_oc')
                 ->label('Оновити Статуси замовлень OpenCart')
                 ->color('success')
-                // ->requiresConfirmation()
                 ->icon('heroicon-o-arrow-down-tray')
                 ->action(function () {
-                    $ocStatuses = OcOrderStatus::where('language_id', 5)->get(); // 5 -укр. яз., 1 - рус. яз.
-                    $statuses = OrderStatus::pluck('Identifier')->toArray();
-                    $forUpdate = [];
-                    $forSave = [];
-                    
-                    foreach($ocStatuses as $ocStatus){
+                    $ocStatuses = OcOrderStatus::where('language_id', 5)->get(); // 5 - укр. яз.
+                    $existingIdentifiers = OrderStatus::pluck('identifier')->toArray();
 
+                    $forSave = [];
+                    $forUpdate = [];
+
+                    foreach ($ocStatuses as $ocStatus) {
                         $data = [
                             'name' => $ocStatus->name ?? '',
                             'store_id' => 1, // ocStore
                             'identifier' => $ocStatus->order_status_id,
-                            'is_active' => 0
+                            'is_active' => 0,
                         ];
-                        if(in_array($ocStatus->order_status_id, $statuses)){
+
+                        if (in_array($ocStatus->order_status_id, $existingIdentifiers)) {
                             $forUpdate[] = $data;
-                        }else {
+                        } else {
                             $forSave[] = $data;
                         }
                     }
 
-                    // Создаем новые статусы
-                    if(!empty($forSave)){
-                        foreach($forSave as $data){
+                    DB::transaction(function () use ($forSave, $forUpdate) {
+                        foreach ($forSave as $data) {
                             OrderStatus::create($data);
                         }
-                    }
 
-                    // Обновляем существующие
-                    foreach ($forUpdate as $dataUpdate){
-                        OrderStatus::updateOrCreate(
-                            ['identifier' => $dataUpdate['identifier']],
-                            [
-                                'name' => $dataUpdate['name'],
-                                'store_id' => $dataUpdate['store_id'],
-                            ]
-                        );
-                    }
-
-                    $forSaveCount = count($forSave);
-                    $forUpdateCount = count($forUpdate);
+                        foreach ($forUpdate as $dataUpdate) {
+                            OrderStatus::updateOrCreate(
+                                ['identifier' => $dataUpdate['identifier']],
+                                [
+                                    'name' => $dataUpdate['name'],
+                                    'store_id' => $dataUpdate['store_id'],
+                                ]
+                            );
+                        }
+                    });
 
                     Notification::make()
                         ->title('Оновлення завершено')
-                        ->body("Додано: {$forSaveCount},\nоновлено: {$forUpdateCount}")
-                        ->success()
-                        ->send();
-
-                 }),    
+                        ->body("Додано: " . count($forSave) . ", оновлено: " . count($forUpdate))
+                        ->success();
+                }), 
 
                  
 
